a1e0c8b1ebda8934c5c2d66b23215657d3d722a8
arfina34800 <arsheeaktar@gmail.com>
2026-03-09 14:25:58 +0600

    secont commit

---
+++ script/login.js
@@ -0,0 +1,12 @@
+document.getElementById("loginBtn").addEventListener("click", function(){
+
+let username = document.querySelector("input[type=text]").value;
+let password = document.querySelector("input[type=password]").value;
+
+if(username === "admin" && password === "admin123"){
+    window.location.href = "home.html";
+}else{
+    alert("Wrong Username or Password");
+}
+
+});
